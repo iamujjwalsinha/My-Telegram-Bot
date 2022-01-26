8a8a8d1d92544e437d6a3d579bfba5c4141e01c9
Ujjwal Sinha <iamujjwalsinha@gmail.com>
2022-01-26 07:25:16 +0000

    added first feature

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+
+var Hi = "hi";
+if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+bot.sendMessage(msg.chat.id,"Hello User");
+}
+
+});
